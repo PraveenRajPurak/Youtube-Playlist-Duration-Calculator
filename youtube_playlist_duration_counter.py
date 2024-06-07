04bccbda18e80a812546abb96ca809e3549f3c26
PraveenRajPurak <95472334+PraveenRajPurak@users.noreply.github.com>
2024-06-07 14:54:17 +0530

    Update youtube_playlist_duration_counter.py

--- youtube_playlist_duration_counter.py
+++ youtube_playlist_duration_counter.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from googleapiclient.discovery import build
 
+# Function to calculate total duration
 def calculate_playlist_duration(playlist_url):
-    api_key = "YOUR API HERE"
+    # Set up API key and build YouTube API service
+    api_key = "AIzaSyBplimmBPwCZVmDICvUa9mYlE0HjrAcejs"
     youtube = build('youtube', 'v3', developerKey=api_key)
-    
+
+    # Extract playlist ID from URL
     playlist_id = playlist_url.split('list=')[1]
-    
+
+    # Get list of videos in the playlist
     playlist_items = youtube.playlistItems().list(
         playlistId=playlist_id,
         part='contentDetails',
-        maxResults=50
+        maxResults=500  # Adjust as per the number of videos in the playlist
     ).execute()
 
     video_ids = [item['contentDetails']['videoId'] for item in playlist_items['items']]
@@ -24,6 +28,7 @@ def calculate_playlist_duration(playlist_url):
 
         duration = video_info['items'][0]['contentDetails']['duration']
 
+        # Parsing duration in ISO 8601 format (PT#H#M#S or PT#M#S or PT#S)
         if 'H' in duration:
             hours_index = duration.find('H')
             hours = int(duration[2:hours_index])
@@ -44,6 +49,7 @@ def calculate_playlist_duration(playlist_url):
 
     return total_duration
 
+# Streamlit UI
 st.title('YouTube Playlist Duration Calculator')
 
 playlist_url = st.text_input('Enter YouTube Playlist URL:')
